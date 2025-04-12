52e436d9325a151bc2ff54121f1fcd5ee36c1927
Shubhankarbhardwaj <shubhankar@Shubhankarbhardwajs-MacBook-Air.local>
2025-04-13 08:34:57 +1200

    Added test case for workshop10

---
+++ tests/tutorial_two/workshop10_troubleshooting.spec.ts
@@ -0,0 +1,11 @@
+import { test, expect } from '@playwright/test';
+
+test.only('Capture screeshot on failure', async ({ page }) => {
+    await page.goto('https://demo.playwright.dev/todomvc/');
+    const flaky = Math.random() < 0.5;
+    console.log(`Flaky value :${flaky}`);
+    if (flaky) {
+        await page.waitForTimeout(300);
+        await page.click('.no-existing-element');
+    }
+})
